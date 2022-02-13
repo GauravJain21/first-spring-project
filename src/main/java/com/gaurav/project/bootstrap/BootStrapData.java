6bc629c105ae6e397891d74e3a077a579abb20f9
Gaurav Jain <gauravjain.p@flipkart.com>
2022-02-13 12:09:15 +0530

    Add spring managed component

---
+++ src/main/java/com/gaurav/project/bootstrap/BootStrapData.java
@@ -0,0 +1,46 @@
+package com.gaurav.project.bootstrap;
+
+import com.gaurav.project.model.Author;
+import com.gaurav.project.model.Book;
+import com.gaurav.project.repositories.AuthorRepository;
+import com.gaurav.project.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "12345");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Developement without EJB", "123456");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+    }
+}
